ab9d1f77116732698f93c71d26fab44c8e60789e
Bekassyl <bekas0108@gmail.com>
2026-02-03 16:45:31 +0500

    added som gsap animations

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -11,25 +11,50 @@ import {Ninth} from "../ninth/Ninth.jsx";
 import Tenth from "../tenth/Tenth.jsx";
 import {Eleventh} from "../eleventh/Eleventh.jsx";
 import {Twelfth} from "../twelfth/Twelfth.jsx";
-
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 export const Main = () => {
+    const mainRef = useRef();
+
+    useGSAP(() => {
+        const sections = mainRef.current.children;
+
+        gsap.utils.toArray(sections).forEach((section) => {
+            gsap.from(section, {
+                y: 100,
+                opacity: 0,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: section,
+                    start: "top 85%",
+                    toggleActions: "play none none reverse",
+                }
+            });
+        });
+    }, { scope: mainRef });
+
     return (
-        <div
-            className="bg-cover"
+        <div ref={mainRef}
+            className="bg-cover bg-fixed"
             style={{ backgroundImage: `url(${bg})` }}
         >
-            <First />
-            <Second />
-            <Third />
-            <Fourth />
-            <Fifth />
-            <Sixth />
-            <Seventh />
-            <Eighth/>
-            <Ninth/>
-            <Tenth/>
-            <Eleventh/>
-            <Twelfth/>
-        </div>
+                <First />
+                <Second />
+                <Third />
+                <Fourth />
+                <Fifth />
+                <Sixth />
+                <Seventh />
+                <Eighth/>
+                <Ninth/>
+                <Tenth/>
+                <Eleventh/>
+                <Twelfth/>
+                <div className="font-ginger flex justify-end"><h5>Suierkulov Bekassyl tg: @uraaasdu</h5></div>
+            </div>
     );
 };
